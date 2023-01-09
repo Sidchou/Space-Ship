c083b2f513b5cdb716c8e7b25ec8d4d18ee87155
Sidchou <sidchou93@gmail.com>
2023-01-09 08:34:42 -0500

    ignore library

--- Library/Bee/artifacts/emscripten/ihi2y/wrapper_emar.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"E:\Install\Unity\2021.3.9f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
-exit %ERRORLEVEL%
--- Library/Bee/artifacts/emscripten/x5q48/wrapper_emcc.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"E:\Install\Unity\2021.3.9f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
-exit %ERRORLEVEL%
